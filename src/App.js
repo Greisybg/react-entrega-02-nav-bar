bc18086395641705d8570fc7158a6be1bbdc8048
Greisybg <95661929+Greisybg@users.noreply.github.com>
2022-06-05 23:00:59 -0300

    entrega 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import Contador from "./components/Contador.jsx";
-import Evento from "./components/Evento.jsx";
-import Parrafo from "./components/Parrafo.jsx";
-import Variables from "./components/Variables.jsx";
-
+import NavBar from "./components/NavBar.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
-      <Parrafo/>
-      <Variables/>
-      <Evento/>
-      <Contador/>
+      <NavBar/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import Link  from "react-router-dom";
+import {ListGroup} from 'react-bootstrap'
 const NavBar = () => {
-  return (
-
-    
-    <div>NavBar</div>
-  )
-}
-
-
-
+    return (
+<ListGroup horizontal>
+  <ListGroup.Item><h3>miniMI</h3></ListGroup.Item>
+  <ListGroup.Item><a href='#'>Ropa</a></ListGroup.Item>
+  <ListGroup.Item><a href='#'>Jugetes</a></ListGroup.Item>
+  <ListGroup.Item><a href='#'>Promociones</a></ListGroup.Item>
+</ListGroup>
+            );
+          };
+          
 
 export default NavBar
